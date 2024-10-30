7776d0a1be23c6eb6a58812a51abec93478c5da7
Dave Richards <davidrichards413@gmail.com>
2024-10-30 17:43:34 -0400

    trying to implement API keys

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // server.js file
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path"); // for handling file path
 const da = require("./data-access");
@@ -13,14 +15,34 @@ app.use(bodyParser.json());
 //app.use(express.static(path.join(__dirname, "public")));
 // Set the static directory to serve files from
 const staticDir = path.join(__dirname, "public");
-console.log(__dirname);
-console.log(staticDir);
+// console.log(__dirname);
+// console.log(staticDir);
 app.use(express.static(staticDir));
+
+//Middleware to check API key
+// const authenticationMiddleware = (res, req, next) => {
+//   const apiKey = req.get("x-api-key"); //NOT WORKING
+//   console.log(`header key = ${apiKey}`); //remove this ASAP
+//   console.log(`env variable = ${process.env.API_KEY}`); //WORKS remove this ASAP
+
+//   if (!apiKey) {
+//     res.status(401); //NOT WORKING res.status is not a function
+//     res.send("API Key is missing");
+//   } else if (apiKey != process.env.API_KEY) {
+//     res.status(403).send("API Key is invalid");
+//   } else {
+//     //API Key is correct
+//     next();
+//   }
+// };
+
+// app.use(authenticationMiddleware);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
+// app.get("/customers", authenticationMiddleware, async (req, res) => {
 app.get("/customers", async (req, res) => {
   const [cust, err] = await da.getCustomers();
   if (cust) {
